531d162887536d3b2a4901898fddf4ef40c3cb65
MarcOut <marc.outteryck@gmail.com>
2022-11-09 00:05:01 +0100

    modification de la structure du code

---
+++ controller.py
@@ -0,0 +1,185 @@
+from datetime import datetime
+
+import views
+from models.tournament import Tournament
+from models.round import Round
+from models.match import Match
+from models.player import Player
+
+
+class Controller:
+    def __init__(self):
+        # Models
+        self.tournament = Tournament()
+        # Views
+        self.view = None
+
+        self.counter_round = 0
+        self.matchs = []
+
+    def run(self):
+        """Lance l'application"""
+        print("Start App")  # A supprimer lors de la finalisation de l'app
+        self.view = views.MainView(self)
+        self.view.screen_tournament()
+        self.view.menu_tournament()
+
+    def add_tournament(self, name: str, location: str, number_rounds: int, number_players: int, time_control: str,
+                       description: str):
+        """Permet de créer un tournoi"""
+        self.tournament.name = name
+        self.tournament.location = location
+        self.tournament.number_rounds = number_rounds
+        self.tournament.number_players = number_players
+        self.tournament.time_control = time_control
+        self.tournament.description = description
+
+    def add_player(self, lastname: str, firstname: str, birthday: str, gender: str, ranking: int):
+        player = Player(lastname=lastname, firstname=firstname, birthday=birthday, gender=gender, ranking=ranking)
+        self.tournament.players.append(player)
+        print(self.tournament.players)
+
+    def sort_list_ranking_and_score(self):
+        self.tournament.players = sorted(self.tournament.players, key=lambda players: players['Classement'],
+                                         reverse=False)
+        self.tournament.players = sorted(self.tournament.players, key=lambda players: players['Score'], reverse=True)
+        return self.tournament.players
+
+    def run_first_round(self):
+        self.matchs = self.launch_first_round()
+        now = datetime.now()
+        name = "1"
+        ending_date = now.strftime("%d %b %Y")
+        ending_time = now.strftime("%Hh%Mm%Ss")
+        round = Round(name=name, ending_date=ending_date, ending_time=ending_time, matchs=self.matchs)
+        self.tournament.round_instance_list(round)
+        self.view.screen_matchs(self.matchs)
+
+    def launch_first_round(self):  # sourcery skip: extract-method
+        self.counter_round += 1
+        if self.counter_round == 1:
+            # création des listes niveaux haut et bas
+            self.sort_list_ranking_and_score()
+            half = len(self.tournament.players) // 2
+            lower_list = self.tournament.players[:half]
+            upper_list = self.tournament.players[half:]
+            matchs = []
+            for i in range(len(lower_list)):
+                match = [upper_list[i], lower_list[i]]
+                matchs.append(match)
+            return matchs
+        else:
+            print("Le premier tour a déjà été effectué! \n"
+                  "Veuillez lancer le tour suivant.")
+            self.counter_round -= 1
+        self.run_first_round()
+
+    def run_next_round(self):
+        players = self.tournament.players[:]
+        self.matchs = self.launch_matchs(players)
+        now = datetime.now()
+        name = f"Round {self.counter_round}"
+        ending_date = now.strftime("%d %b %Y")
+        ending_time = now.strftime("%Hh%Mm%Ss")
+        round = Round(name=name, ending_date=ending_date, ending_time=ending_time, matchs=self.matchs)
+        self.tournament.round_instance_list(round)
+        self.view.screen_matchs(self.matchs)
+
+    def launch_matchs(self, players, matchs=None):
+        # sourcery skip: extract-duplicate-method
+        if matchs is None:
+            matchs = []
+        self.counter_round += 1
+        if self.counter_round > self.tournament.number_rounds:
+            print("Le tournoi est fini")
+        elif self.counter_round <= 1:
+            print("Veuillez lancer le 1er round")
+            self.view.menu_tournament()
+        else:
+            while players:
+                match = [players[0], players[1]]
+                if match not in self.tournament.round_instance:
+                    matchs.append(match)
+                    players.remove(players[0])
+                    players.remove(players[0])
+                    self.launch_matchs(players, matchs)
+                else:
+                    match = [players[0], players[2]]
+                    if match not in self.tournament.round_instance:
+                        matchs.append(match)
+                        players.remove(players[0])
+                        players.remove(players[1])
+                        self.launch_matchs(players, matchs)
+                    if self.counter_round == self.tournament.number_rounds:
+                        print("Le tournoi est fini")
+            return matchs
+
+    def run_menu_result(self):  # sourcery skip: extract-method
+        choice = self.view.get_result(self.matchs)
+        try:
+            choice_int = int(choice)
+            if choice_int in range(1, len(self.matchs)+1):
+                player_1 = self.matchs[choice_int - 1][0]["Nom de famille"]
+                player_2 = self.matchs[choice_int - 1][1]["Nom de famille"]
+                choice = self.view.enter_result(player_1=player_1, player_2=player_2)
+                score_player_1, score_player_2 = self.enter_result(choice=choice)
+                player_1_with_score, player_2_with_score = self.result(player_1=player_1, player_2=player_2,
+                                                                       score_player_1=score_player_1,
+                                                                       score_player_2=score_player_2)
+                match_result = self.get_match_result(player_1=player_1_with_score, player_2=player_2_with_score)
+                winner = self.add_score(player_1=player_1_with_score, player_2=player_2_with_score)
+                match = Match(player_1=player_1, player_2=player_2, match_result=match_result, winner=winner)
+            elif choice_int == 0:
+                self.view.menu_tournament()
+            self.run_menu_result()
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+            self.run_menu_result()
+
+    def enter_result(self, choice: int):
+        try:
+            score_player_1 = 0
+            score_player_2 = 0
+            if choice == 1:
+                score_player_1 = 1
+                score_player_2 = 0
+            elif choice == 2:
+                score_player_1 = 0
+                score_player_2 = 1
+            elif choice == 3:
+                score_player_1 = 0.5
+                score_player_2 = 0.5
+            return score_player_1, score_player_2
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+            self.run_menu_result()
+
+    def result(self, player_1: str, player_2: str, score_player_1: float, score_player_2: float):
+        player_1 = [player_1, score_player_1]
+        player_2 = [player_2, score_player_2]
+        return player_1, player_2
+
+    def get_match_result(self, player_1: list, player_2: list):
+        match_result = (player_1, player_2)
+        match_result = tuple(match_result)
+        return match_result
+
+    def add_score(self, player_1, player_2):
+        # sourcery skip: extract-duplicate-method, inline-immediately-returned-variable
+        winner = ""
+        for player in self.tournament.players:
+            if player["Nom de famille"] == player_1[0]:
+                player["Score"] += player_1[1]
+                winner = player_1[0]
+            if player["Nom de famille"] == player_2[0]:
+                player["Score"] += player_2[1]
+                winner = player_2[0]
+            return winner
+
+    def get_report(self, choice):
+        if choice == 1:
+            pass
+        elif choice == 2:
+            self.view.screen_ranking()
+        elif choice == 3:
+            self.view.screen_all_rounds()
---
+++ views.py
@@ -0,0 +1,183 @@
+import datetime
+
+
+class MainView():
+
+    def __init__(self, controller):
+        self.controller = controller
+        self.main_menu()
+
+    def main_menu(self):
+        """Menu principal"""
+        while True:
+            choice = input(" 1. Créer un tournoi \n"
+                           " 2. Ajouter les joueurs au tournoi \n"
+                           " 3. Charger un tournoi\n"
+                           " 4. Editer un rapport\n"
+                           " 5. Mettre à jour le classement\n"
+                           " 6. Exit \n"
+                           "--> ")
+            try:
+                choice_int = int(choice)
+                if choice_int == 1:
+                    self.create_tournament()
+                elif choice_int == 2:
+                    self.add_player()
+                    break
+                elif choice_int == 3:
+                    break
+                elif choice_int == 4:
+                    self.menu_get_report()
+            except ValueError:
+                print("Veuillez répondre par un chiffre correspondant à votre choix.")
+
+    def screen_tournament(self):
+        """Affiche les informations du tournoi qui vient d'être créé"""
+        print("Félicitation, vous avez créer votre un Tournoi")
+
+    def create_tournament(self):
+        """ Vue pour la création du tournoi"""
+        name = input("Nom du tournoi: ")
+        location = input("Lieu du tournoi: ")
+        number_rounds = self.get_number_rounds()
+        # tournée = ??
+        number_players = self.get_number_players()
+        time_control = self.get_time_control()
+        description = input("Veuillez ajouter la description du tournoi: ")
+        self.controller.add_tournament(name=name, location=location, number_rounds=number_rounds,
+                                       number_players=number_players, time_control=time_control,
+                                       description=description)
+
+    def get_number_players(self):
+        try:
+            return int(input("Quelle est le nombre de joueurs: "))
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+
+    def get_number_rounds(self):
+        try:
+            return int(input("Quelle est le nombre de tours: "))
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+
+    def get_time_control(self):
+        """Permet de récupérer le type de contrôle de temps"""
+        time_control = input("Quelle est le type de contrôle de temps: \n"
+                             "1. Bullet / 2. Blitz / 3. Coup rapide\n"
+                             "--> ")
+        try:
+            time_control_int = int(time_control)
+            if time_control_int == 1:
+                return "Bullet"
+            elif time_control == 2:
+                return "Blitz"
+            elif time_control_int == 3:
+                return "Coup rapide"
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+
+    def add_player(self):
+        """Vue pour l'ajout d'un joueur'"""
+        for _ in range(int(self.controller.tournament.number_players)):
+            lastname = input("Ajouter le nom de famille:")
+            firstname = input("Ajouter le prénom: ")
+            birthday = self.get_birthday()
+            gender = self.get_gender()
+            ranking = input("Ajouter le classement : 00")
+            self.controller.add_player(lastname=lastname, firstname=firstname, birthday=birthday, gender=gender,
+                                       ranking=ranking)
+
+    def get_birthday(self):
+        """Permet de demander la date de naissance et de vérifier son format"""
+        date_format = "%d-%m-%Y"
+        birthday = input("Ajouter la date de naissance: jj-mm-aaaa")
+        try:
+            valid_date = datetime.datetime.strptime(birthday, date_format)
+            return birthday
+        except ValueError:
+            print("Date non valide! ", birthday)
+            self.get_birthday()
+
+    def get_gender(self):
+        """Permet de demander le sexe"""
+        get_gender = input("Quelle est le sexe?\n" "1. Féminin / 2. Masculin \n")
+        try:
+            get_gender_int = int(get_gender)
+            if get_gender_int == 1:
+                return "Féminin"
+            elif get_gender_int == 2:
+                return "Masculin"
+        except ValueError:
+            print("Veuillez répondre par un chiffre correspondant à votre choix.")
+            self.get_gender()
+
+    def menu_tournament(self):
+        """Menu du tournoi"""
+        while True:
+            choice = input("\n\n 1. Lancer le 1er tour \n"
+                           " 2. Lancer le tour suivant \n"
+                           " 3. Entrer les résultats des matchs\n"
+                           " 4. Afficher le classement\n"
+                           " 5. Exit \n"
+                           "--> ")
+            try:
+                choice_int = int(choice)
+                if choice_int == 1:
+                    self.controller.run_first_round()
+                elif choice_int == 2:
+                    self.controller.run_next_round()
+                elif choice_int == 3:
+                    self.controller.run_menu_result()
+                elif choice_int == 4:
+                    self.screen_ranking()
+            except ValueError:
+                print("Veuillez répondre par un chiffre correspondant à votre choix.")
+
+    def screen_matchs(self, matchs):
+        print("\nLes matchs pour ce tour sont : ")  # Est-ce que je dois le mettre dans la vue
+
+        for match in matchs:
+            print(f"Mme/M {match[0]['Nom de famille']} vs Mme/M {match[1]['Nom de famille']}", end="\n")
+
+    def get_result(self, matchs):
+        print(" Veuillez choisir le match afin de rentrer ses résultats")
+        for match in matchs:
+            print(
+                f"{matchs.index(match) + 1}. Mme/M {match[0]['Nom de famille']} VS Mme/M {match[1]['Nom de famille']}")
+        print("0. Précédent")
+        return input("\nVeuillez entrez votre choix : \n")
+
+    def enter_result(self, player_1, player_2):
+        print("Quelle est le résultat de la partie ? \n"
+              f"Si M/Mme {player_1} a gagné(e), tapé 1\n"
+              f"Si M/Mme {player_2} a gagné(e), tapé 2\n"
+              f"Si match nul, tapé 3")
+        return input("Match result : Veuillez indiquer votre choix : ")
+
+    def screen_ranking(self):
+        players = self.controller.sort_list_ranking_and_score()  # est-ce que je dois créer une fonction ?
+        for player in players:
+            print(f"Nom de famille : {player['Nom de famille']}, classé: {player['Classement']} "
+                  f"avec un score de : {player['Score']} ")
+
+    def menu_get_report(self):
+        print("Affichage des rapports\n")
+        while True:
+            print(
+                "1 - Rapport de tous les acteurs \n"
+                "2 - Liste de tous les joueurs du tournoi\n"
+                "3 - Liste de tous les tournois\n"
+                "4 - Liste de tous les tours d'un tournoi"
+                "5 - Liste de tous les matchs d'un tournoi")
+
+            choice = input("\nVeuillez entrez votre choix : \n")
+            try:
+                choice_int = int(choice)
+
+            except ValueError:
+                print("Veuillez répondre par un chiffre correspondant à votre choix.")
+                self.main_menu()
+
+    def screen_all_rounds(self):
+        for round in self.controller.tournament.round_instance:
+            print(round)
